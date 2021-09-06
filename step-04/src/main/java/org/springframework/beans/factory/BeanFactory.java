f094a6f2bf6e225a9e405c5420fc239ad66ff82a
liushan03 <liushan03@meicai.cn>
2021-09-06 16:00:58 +0800

    study small-spring 4 填充属性/对象

---
+++ step-04/src/main/java/org/springframework/beans/factory/BeanFactory.java
@@ -0,0 +1,13 @@
+package org.springframework.beans.factory;
+
+import org.springframework.beans.BeansException;
+
+/**
+ * @Author: Liushan
+ * @Date: 2021/9/3 18:28
+ */
+public interface BeanFactory {
+    Object getBean(String name) throws BeansException;
+
+    Object getBean(String name, Object... args) throws BeansException;
+}
---
+++ step-04/src/test/java/org/springframework/test/bean/UserService.java
@@ -0,0 +1,31 @@
+package org.springframework.test.bean;
+
+/**
+ * @Author: Liushan
+ * @Date: 2021/9/6 11:01
+ */
+public class UserService {
+    private String uId;
+
+    private UserDao userDao;
+
+    public void queryUserInfo() {
+        System.out.println("查询用户信息：" + userDao.queryUserName(uId));
+    }
+
+    public String getuId() {
+        return uId;
+    }
+
+    public void setuId(String uId) {
+        this.uId = uId;
+    }
+
+    public UserDao getUserDao() {
+        return userDao;
+    }
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+}
